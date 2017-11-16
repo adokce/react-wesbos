fa2dc9da4d0658a01870cdd3045cff8c4c8f879c
Adnan Karsic <adnankarsic@outlook.com>
2017-11-16 10:37:34 +0100

    menaging orders orders done and adding firebase started

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDVLhEojSKtcZnRcuaf3ZjrHMAige5OR4c",
+    authDomain: "catch-of-the-day-adokce.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-adokce.firebaseio.com",
+
+})
+
+export default base
